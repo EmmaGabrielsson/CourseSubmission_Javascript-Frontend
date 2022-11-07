a012f411dcd04f01bab9c99c5c46a9d41dd5efe6
Emma Gabrielsson <111791152+dreamworks0601@users.noreply.github.com>
2022-11-07 23:47:53 +0100

    försöker få till funktioner för att hantera kassan

--- script/handleCart.js
+++ script/handleCart.js
@@ -1,11 +1,9 @@
-import * as api from "./api-fetch-products.js";
+import { productData } from "./api-fetch-products.js";
 
 const showCart = document.querySelector("#cart-icon");
 const cart = document.querySelector(".cart");
 const closeCartBtn = document.querySelector("#close-cart");
-const addToCart = document.querySelectorAll(".add-cart");
 const showAddedProducts = document.querySelector(".cart-content");
-const removeFromCart = document.querySelectorAll(".cart-remove");
 
 function openCart() {
     cart.classList.add("active");
@@ -17,42 +15,53 @@ function closeCart() {
 }
 closeCartBtn.addEventListener("click", closeCart);
 
-function showStoredProductsInCart() {
-    let productId;
-    let storedProduct;
+
+//visa sparade produkter från localstorage i kassan
+export function showStoredProductsInCart() {
     if (localStorage.length == 0) {
         showAddedProducts.innerText = "No products added to cart yet!";
         showAddedProducts.style.textAlign = "center";
     }
     else {
-        Object.keys(localStorage).forEach(function(key) {
-            productId = localStorage.getItem(key);
-            storedProduct = JSON.parse(productId);
-            if (storeProducts) {
-                let contentContainer = document.createElement("div");
-                contentContainer.classList.add("cart-box");
-                let productContent = `
-                <img src="${storedProduct[1]}" alt="" class="cart-img">
+        let contentContainer = document.createElement("div");
+        contentContainer.classList.add("cart-box");
+        let productContent = `
+                <img src="#" alt="" class="cart-img">
                 <div class="detail-box">
-                <div class="cart-title-product">${storedProduct[2]}</div>
-                <div class="cart-price">${storedProduct[3]}</div>
+                <div class="cart-title-product">${title}</div>
+                <div class="cart-price">${price}</div>
                 <label for="quantity">Antal</label>
                 <input name="quantity" title="quantity" type="number" value="1" class="cart-quantity">
                 </div>
-                <i class="bi bi-trash cart-remove" title="delete from cart"></i>`;
-                
-                contentContainer.appendChild(productContent);
-                showAddedProducts = productContent.innerHTML;
-            }
-        });
+                <i class="bi bi-trash cart-remove" title="delete from cart"></i>
+                `;
+
+        contentContainer.appendChild(productContent);
+        showAddedProducts = productContent.innerHTML;
     }
 }
 
-function storeProducts() {
-    localStorage.setItem(productData[i].id, JSON.stringify());
+
+//spara added product i localstorage
+/*function storeProducts() {
+    localStorage.setItem(addBtnClicked(), JSON.stringify(storeData));
     showStoredProductsInCart();
 }
 
-addToCart.forEach.click = storeProducts;
-showStoredProductsInCart();
+function addBtnClicked() {
+    storeProducts();
+}
 
+/*
+function addBtnClicked() {
+    addBtn.addEventListener("click", storeProducts);
+}
+if (localStorage == !productData) {
+    storeProducts();
+}
+else if (localStorage == productData) {
+    alert("This product is already added to cart.");
+}*/
+
+//uppdatera antal och summa i kassa
+//updateCart();*/
